ca82899eb0584b75733998a5661a85913112a854
marianazleslie <marianazleslie@gmail.com>
2025-08-31 11:51:40 -0300

    projeto criado

---
+++ index.js
@@ -0,0 +1,10 @@
+    var nome;
+    var xp;
+    nome = prompt('ingresse nome de Herói:');
+    xp = prompt('ingresse quantidade de experiência (XP) de herói:');
+
+    switch(xp)
+    {   case "xp<1000":
+        console.log("Ferro")
+        break;
+    }
