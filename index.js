aa060df3ea28502749e39cc11e4bd71e015feaf5
marianazleslie <marianazleslie@gmail.com>
2025-08-31 13:50:53 -0300

    terminado

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-    var nome;
-    var xp;
-    nome = prompt('ingresse nome de Herói:');
-    xp = prompt('ingresse quantidade de experiência (XP) de herói:');
+   let nome= "mariana";
+   let xp=7003;
 
-    switch(xp)
-    {   case "xp<1000":
-        console.log("Ferro")
-        break;
-    }
+     if(xp<1000)    console.log("O Herói de "+ nome +" está no nível de Ferro");
+else if(1001<xp && xp<=2000)  console.log("O Herói de "+ nome +" está no nível de Bronze");
+ else if(xp>2001 && xp<=5000)  console.log("O Herói de "+ nome +" está no nível de Prata");
+ else if(xp>5001 && xp<=7000)  console.log("O Herói de "+ nome +" está no nível de Ouro");
+else if(xp>7001 && xp<=8000)  console.log("O Herói de "+ nome +" está no nível de Platina");
+else if(xp>8001 && xp<=9000)  console.log("O Herói de "+ nome +" está no nível de Ascendente");
+else if(xp>9001 && xp<=10000)  console.log("O Herói de "+ nome +" está no nível de Imortal");
+ else if(10001>xp)  console.log("O Herói de "+ nome +" está no nível de Radiante");
